0a4f2f36a14d6b989b24bf9b8c6b69cc998516a0
luyuan <18917963668@163.com>
2021-12-20 22:05:25 +0800

    LeetCode

--- src/sortDemo/compare/merge.go
+++ src/sortDemo/compare/merge.go
@@ -13,6 +13,7 @@ func MergeConstruct(a []int) {
 	merge.sort(a, 0, len(a)-1)
 }
 
+// 拆分切片直到不能切为止
 func (m *Merge) sort(a []int, lo int, hi int) {
 	if hi <= lo {
 		return
@@ -30,15 +31,19 @@ func (m *Merge) merge(a []int, lo int, mid int, hi int) {
 		m.Aux[i] = a[i]
 	}
 	for i := lo; i <= hi; i++ {
+		// 左边用完，取右边
 		if left > mid {
 			m.Arr[i] = m.Aux[right]
 			right++
+			// 右边用完取左边
 		} else if right > hi {
 			m.Arr[i] = m.Aux[left]
 			left++
+			// 右边小于左边，放右边
 		} else if m.less(right, left) {
 			m.Arr[i] = m.Aux[right]
 			right++
+			// 左边小于右边放左边
 		} else {
 			m.Arr[i] = m.Aux[left]
 			left++
